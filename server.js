57dc878c25d21bdc18153d30ef0d164f72a0892f
Ben Kasper <bckasper@gmail.com>
2021-10-17 19:52:54 -0400

    added application requirements and database connections in the server file

---
+++ server.js
@@ -0,0 +1,17 @@
+// Application requirements
+const mysql = require('mysql2')
+const cTable = require('console.table')
+const inquirer = require('inquirer')
+require('dotenv').config()
+
+// Connection to the database
+const db = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
+)
+
