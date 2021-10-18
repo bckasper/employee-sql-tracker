a1732737a4b1edca20a764f6dc819bd91cd7b230
Ben Kasper <bckasper@gmail.com>
2021-10-17 20:27:10 -0400

    added a createAppHeader function to make a header appear when the application is launched; also wrote the first part of the initial inquirer prompt

--- server.js
+++ server.js
@@ -8,10 +8,47 @@ require('dotenv').config()
 const db = mysql.createConnection(
     {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
-    }
+    },
 )
+    
+db.connect(err => {
+    if(err){console.log(err)}       
+    else {console.log('Connected to the employees_db database.')}
+    createAppHeader();
+    initialize();
+})
 
+// Make a header in the terminal for the application
+function createAppHeader(){
+    console.log(`_______________________`)
+    console.log(`|                     |`)
+    console.log(`|      EMPLOYEE       |`)
+    console.log(`|     MANAGEMENT      |`)
+    console.log(`|       SYSTEM        |`)
+    console.log(`|_____________________|`)
+    console.log(`                       `)
+    console.log(`                       `)
+    console.log(`                       `)
+}
+
+const initialize = () => {
+    inquirer.prompt(
+        {
+            type: 'list',
+            name: 'prompt',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update and employee role',
+            ]
+        }
+    )
+}
